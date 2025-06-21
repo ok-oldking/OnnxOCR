292dd2319e54178e9ea194af7ae6b2d115f6ebe7
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2025-06-21 08:13:38 +0800

    change default values

--- onnxocr/utils.py
+++ onnxocr/utils.py
@@ -277,8 +277,8 @@ def infer_args():
 
     # DB parmas
     parser.add_argument("--det_db_thresh", type=float, default=0.3)
-    parser.add_argument("--det_db_box_thresh", type=float, default=0.6)
-    parser.add_argument("--det_db_unclip_ratio", type=float, default=1.5)
+    parser.add_argument("--det_db_box_thresh", type=float, default=0.5)
+    parser.add_argument("--det_db_unclip_ratio", type=float, default=1.6)
     parser.add_argument("--max_batch_size", type=int, default=10)
     parser.add_argument("--use_dilation", type=str2bool, default=False)
     parser.add_argument("--det_db_score_mode", type=str, default="fast")
